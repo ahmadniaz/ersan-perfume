7ab4daa63143114005cac041f97a94dd607adebb
ahmadniaz <ahmadniazpk123@gmail.com>
2023-02-27 16:02:01 +0500

    working on search component

---
+++ src/components/search/Search.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+
+const Search=()=>{
+    return(
+        <>
+        <div className="search_container">
+            <div className="search_box">
+                <div className=""></div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Search;
